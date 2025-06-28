6b1abd7cb5e321b548e11b4d1dec27bd8339568e
Kanhaiya-byte <kanhaiyayadav0003@gmail.com>
2025-06-28 20:19:39 +0530

    Update API URL to deployed backend

--- client/script.js
+++ client/script.js
@@ -137,10 +137,13 @@ form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(form);
 
-  const res = await fetch("http://localhost:5000/submit-profile", {
-    method: "POST",
-    body: formData,
-  });
+  const res = await fetch(
+    "https://user-profile-form-2.onrender.com/submit-profile",
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   const data = await res.json();
   alert(data.message);
